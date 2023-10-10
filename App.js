0fb0be38c03f35fe8c1bf58c04fe30c3d01d8851
NguyenHieu17 <hieu92145@gmail.com>
2023-10-10 14:24:26 +0700

    hoan hanh 1-3 man hinh

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginApp from './view/LoginApp';
+import CheckPassword from './view/CheckPassword';
+import ProductReview from './view/productReview';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <AppOne/>
+    // <LoginApp/>
+    <CheckPassword/>
+    // <ProductReview/>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
